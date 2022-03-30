54a3fc80a22e11903a2e07fcf9d8d538c4d25e9b
Ashley Young <asyoung@redhat.com>
2022-03-30 09:15:44 -0400

    adding sample config file. adding some comments

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # st_dbConf
-Shared python module for reading database config files, handling db queries, and generic flask configuration
+Shared python module for reading database config files, handling db queries, and generic flask configuration, formatting currency and potentially some work around hourly rates
+
+installed via:
+ls -la /usr/local/lib64/python3.6/site-packages/
--- __init__.py
+++ __init__.py
@@ -1,28 +1,55 @@
 import configparser as conf
-from flask import jsonify
 import pymysql
 
+
+class currencyUSD:
+    '''
+    Ensures that currency stuff is returned in a currency friendly format
+    even if null, none, empty, etc
+    '''
+
+    def default(amount):
+        if amount is None:
+            return ("%0.2f" % float(0))
+        else:
+            return ("%0.2f" % float(amount))
+
+
 def baseConfig():
+    '''
+    Setup basic things we need for flask, including where the config file is
+
+    Currently, all flask apps use the same appconfig and key so that authenication
+    can be shard across different flask based services.
+    '''
+
     configParser = conf.RawConfigParser()
     configFilePath = r'/etc/skatetrax/settings.conf'
     configParser.read(configFilePath)
     appConfig = configParser.get('appKey', 'secret')
     return appConfig
 
-def dbconnect(sql,vTUP=None):
-   configParser = conf.RawConfigParser()
-   configFilePath = r'/etc/skatetrax/settings.conf'
-   configParser.read(configFilePath)
 
-   host = configParser.get('dbconf', 'host')
-   user = configParser.get('dbconf', 'user')
-   password = configParser.get('dbconf', 'password')
-   db = configParser.get('dbconf', 'db')
+def dbconnect(sql, vTUP=None):
+    '''
+    Since all apps are using the same database settings, set them up here too
+    '''
 
-   con = pymysql.connect(host=host, user=user, password=password, db=db, cursorclass=pymysql.cursors.DictCursor, autocommit=True)
-   cur = con.cursor()
-   cur.execute(sql, vTUP)
-   tables = cur.fetchall()
-   cur.connection.commit()
-   con.close()
-   return tables
+    configParser = conf.RawConfigParser()
+    configFilePath = r'/etc/skatetrax/settings.conf'
+    configParser.read(configFilePath)
+
+    host = configParser.get('dbconf', 'host')
+    user = configParser.get('dbconf', 'user')
+    password = configParser.get('dbconf', 'password')
+    db = configParser.get('dbconf', 'db')
+
+    con = pymysql.connect(host=host, user=user, password=password,
+                          db=db, cursorclass=pymysql.cursors.DictCursor,
+                          autocommit=True)
+    cur = con.cursor()
+    cur.execute(sql, vTUP)
+    tables = cur.fetchall()
+    cur.connection.commit()
+    con.close()
+    return tables
